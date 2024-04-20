157e7a50c310db4543e728b5e87ab783867e97e0
Pawankumar Pandey <pawanpandey86.20@gmail.com>
2024-04-20 14:54:05 +0530

    code added

---
+++ models/transectionModel.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+const transectionSchema = new mongoose.Schema({
+
+    userid:{
+        type:String,
+        required:true,
+
+    },
+    amount : {
+        type:Number,
+        required:[true,'amount is required']
+    },
+
+    type:{
+        type:String,
+        required:[true,'Type is required']
+    },
+
+    category:{
+        type:String,
+        required:[true,'Category is required']
+    },
+    reference:{
+        type:String,
+        required:false
+    },
+    description:{
+        type:String,
+        required:[true,'Description is required']
+    },
+    date:{
+        type:Date,
+        required:[true,'Data is required']
+    },
+    },
+    {timestamps:true}
+);
+
+
+const transectionModel = mongoose.model('transections', transectionSchema)
+module.exports = transectionModel;
---
+++ server.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const colors = require("colors");
+const path = require("path");
+const connectDb = require("./config/connectDB");
+// const connectDb = require("./config/connectDB.js");
+//config dot env file
+dotenv.config();
+
+//database call
+connectDb();
+
+//rest object
+const app = express();
+
+//middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors());
+
+//routes
+//users routes
+app.use(('/api/v1/users'),require('./routes/userRoute'));
+//transection routes
+app.use('/api/v1/transections', require("./routes/transectionRoutes"));
+
+//static Files
+app.use(express.static(path.join(__dirname,"./client/build")))
+
+app.get('*',function(req,res){
+    res.sendFile(path.join(__dirname,"./client/build/index.html"))
+})
+
+//port
+const PORT = process.env.PORT || 8080;
+
+//listen server
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require("express");
+// const cors = require("cors");
+// const morgan = require("morgan");
+// const dotenv = require("dotenv");
+// const colors = require("colors");
+// const connectDb = require("./config/connectDB.js");
+// //config dot env file
+// dotenv.config();
+
+// //database call
+// connectDb();
+
+// //rest object
+// const app = express();
+
+// //middlewares
+// app.use(morgan('dev'));
+// app.use(express.json());
+// app.use(cors());
+
+// //routes
+// app.get('/',(req,res)=>{
+//     res.send("<h1>Hello from server</h1>");
+// })
+
+// //port
+// const PORT = 8080 || process.env.PORT;
+
+// //listen server
+// app.listen(PORT,()=>{
+//     console.log(`Server running on port ${PORT}`);
+// })
+
